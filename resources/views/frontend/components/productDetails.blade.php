e3e1e50639cdb29987a642b55b26ea0ddec57fc4
zidankhan86 <zidankhan718@gmail.com>
2024-07-19 21:35:25 +0600

    fix: Remove extra CSS

--- resources/views/frontend/components/productDetails.blade.php
+++ resources/views/frontend/components/productDetails.blade.php
@@ -149,35 +149,7 @@
         </div>
     </section>
     <!-- Product Details Section End -->
-    <style>
-        /* rating */
-    .rating-css div {
-        color: #ffe400;
-        font-size: 5px;
-        font-family: sans-serif;
-        font-weight: 300;
-        text-align: ;
-        text-transform: uppercase;
-        padding: 10px 0;
-      }
-      .rating-css input {
-        display: none;
-      }
-      .rating-css input + label {
-        font-size: 25px;
-        text-shadow: 1px 1px 0 #8f8420;
-        cursor: pointer;
-      }
-      .rating-css input:checked + label ~ label {
-        color: #b4afaf;
-      }
-      .rating-css label:active {
-        transform: scale(0.8);
-        transition: 0.3s ease;
-      }
-
-    /* End of Star Rating */
-    </style>
+    
 
    </div>
          </div>
@@ -203,7 +175,7 @@
         display: none;
       }
       .rating-css input + label {
-        font-size: 25px;
+        font-size: 15px;
         text-shadow: 1px 1px 0 #8f8420;
         cursor: pointer;
       }
