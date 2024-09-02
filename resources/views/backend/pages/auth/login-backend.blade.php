6a1499c56b3c5f3fe15612cca06b5851cb21256d
zidankhan86 <zidankhan718@gmail.com>
2024-09-03 02:40:34 +0600

    fix: login

--- resources/views/backend/pages/auth/login-backend.blade.php
+++ resources/views/backend/pages/auth/login-backend.blade.php
@@ -1,43 +1,39 @@
 @extends('backend.pages.auth.loginForm')
 
 @section('loginbackend')
-<div class="card shadow-lg border-0 rounded-lg mt-5">
-    <div class="card-header bg-primary text-white">
-        <h3 class="text-center font-weight-light my-4">Login</h3>
-    </div>
-    <div class="card-body">
-        <form action="{{route('login.submit')}}" method="post" enctype="multipart/form-data">
-            @csrf
 
-            @if(session('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
+<div class="login-form-bg h-100">
+    <div class="container h-100">
+        <div class="row justify-content-center h-100">
+            <div class="col-xl-6">
+                <div class="form-input-content">
+                    <div class="card login-form mb-0">
+                        <div class="card-body pt-5">
+                            <a class="text-center" href="index.html"> <h4>Furniture</h4></a>
+    
+                            <form action="{{route('login.submit')}}" method="post" enctype="multipart/form-data">
+                                @csrf
+                    
+                                @if(session('error'))
+                                <div class="alert alert-danger">
+                                    {{ session('error') }}
+                                </div>
+                                @endif
+                                <div class="form-group">
+                                    <input name="email" value="{{old('email')}}" type="email" class="form-control" placeholder="Email">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="password" id="inputPassword" value="{{old('password')}}" class="form-control" placeholder="Password">
+                                </div>
+                                <button type="submit" class="btn login-form__btn submit w-100">Sign In</button>
+                            </form>
+                            <div class="small text-right"><br>
+                                Click <a href="{{route('home')}}">here</a> to go home
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            @endif
-
-            <div class="form-floating mb-3">
-                <input class="form-control" id="inputEmail" name="email" value="{{old('email')}}" type="email" placeholder="name@example.com" />
-                <label for="inputEmail">Email address</label>
-            </div>
-            <div class="form-floating mb-3">
-                <input class="form-control" name="password" id="inputPassword" value="{{old('password')}}" type="password" placeholder="Password" />
-                <label for="inputPassword">Password</label>
-            </div>
-            <div class="form-check mb-3">
-                <input class="form-check-input" id="inputRememberPassword" type="checkbox" value="" />
-                <label class="form-check-label" for="inputRememberPassword">Remember Password</label>
-            </div>
-            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                <button class="btn btn-primary" style="color:black; width: 100%;" type="submit">
-                    <i class="fas fa-sign-in-alt"></i> Login
-                </button>
-            </div>
-        </form>
-    </div>
-
-    <div class="card-footer bg-light py-3">
-        <div class="small text-right">
-            Click <a href="{{route('home')}}">here</a> to go <i class="fa fa-home btn btn-success"></i>
         </div>
     </div>
 </div>
--- resources/views/backend/pages/auth/loginForm.blade.php
+++ resources/views/backend/pages/auth/loginForm.blade.php
@@ -1,55 +1,63 @@
+
+
+
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html class="h-100" lang="en">
+
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Login - {{ config('app.name') }}</title>
-    <link href="css/styles.css" rel="stylesheet" />
-    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Quixlab - Bootstrap Admin Dashboard Template by Themefisher.com</title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="../../assets/images/favicon.png">
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous"> -->
+    <link href="css/style.css" rel="stylesheet">
+    
 </head>
-<body class="bg-custom">
-    <div id="layoutAuthentication">
-        <div id="layoutAuthentication_content">
-            <main>
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-lg-5">
-                            @notifyCss
-                            @yield('loginbackend')
-                            @include('notify::components.notify')
-                            @notifyJs
-                        </div>
-                    </div>
-                </div>
-            </main>
-        </div>
-        <div id="layoutAuthentication_footer">
-            <footer class="py-4 bg-light mt-auto">
-                <div class="container-fluid px-4">
-                    <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright &copy; {{ config('app.name') }} {{ date('Y') }}</div>
-                    </div>
-                </div>
-            </footer>
+
+<body class="h-100">
+    
+    <!--*******************
+        Preloader start
+    ********************-->
+    <div id="preloader">
+        <div class="loader">
+            <svg class="circular" viewBox="25 25 50 50">
+                <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3" stroke-miterlimit="10" />
+            </svg>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script src="js/scripts.js"></script>
+    <!--*******************
+        Preloader end
+    ********************-->
+
+    
+
+    @notifyCss
+    @yield('loginbackend')
+    @include('notify::components.notify')
+    @notifyJs
+
+
+    
+
+    
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <script src="plugins/common/common.min.js"></script>
+    <script src="js/custom.min.js"></script>
+    <script src="js/settings.js"></script>
+    <script src="js/gleek.js"></script>
+    <script src="js/styleSwitcher.js"></script>
 </body>
 </html>
 
 
-<style>
-    .bg-custom {
-    background-color: #16a782; /* Replace with your desired background color */
-    /* Optionally add a background image or gradient */
-     background-image: url('frontend/src/images/banner/banner-lg-03.jpg'); 
-    /* background: linear-gradient(to right, #ffffff, #f0f4f8); */
-    background-size: cover;
-    background-repeat: no-repeat;
-}
 
-</style>
+
+
