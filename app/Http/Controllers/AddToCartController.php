18adf7b783ca61bdaa47525b48866551292321fd
zidankhan86 <zidankhan718@gmail.com>
2024-09-09 02:39:28 +0600

    fix: add to cart

--- app/Http/Controllers/AddToCartController.php
+++ app/Http/Controllers/AddToCartController.php
@@ -9,65 +9,7 @@ use RealRashid\SweetAlert\Facades\Alert;
 
 class AddToCartController extends Controller
 {
-    public function addToCart($id)
-    {
-
-        $product=Product::find($id);
-        if($product)
-        {
-            $cart=session()->get('cart');
-//            dd($cart);
-            if(!$cart)
-            {
-                //step 1: cart empty
-                //add to cart- first product
-                    $myCart[$id]=[
-                      'name'=>$product->name,
-                      'price'=>$product->price,
-                      'quantity'=>1,
-                      'subtotal'=>$product->price,//price x quantity
-                    ];
-
-                  session()->put('cart',$myCart);
-                  Alert::toast()->success('Product added to the cart');
-
-                  return redirect()->back();
-            }
-
-
-            //step 2:Cart not empty but product not exist
-            //add to cart
-
-            if(!array_key_exists($id,$cart)){
-                $cart[$id]=[
-                    'name'=>$product->name,
-                    'price'=>$product->price,
-                    'quantity'=>1,
-                    'subtotal'=>$product->price,//price x quantity
-                ];
-
-                session()->put('cart',$cart);
-                Alert::toast()->success('New product added to the cart');
-                return redirect()->back();
-
-            }
-
-            //step 3 : cart not empty but product exist
-            // quantity , subtotal update
-            $cart[$id]['quantity']=$cart[$id]['quantity']+1;
-            $cart[$id]['subtotal']=$cart[$id]['quantity'] * $cart[$id]['price'];
-            session()->put('cart',$cart);
-
-            Alert::toast()->success('Cart successfully updated.');
-            return redirect()->back();
-          }
-
-          Alert::toast()->success('No Product Found.');
-          return redirect()->back();
-
-
-
-            }
+   
 
             public function viewCart()
             {
@@ -109,5 +51,63 @@ class AddToCartController extends Controller
 
                   return redirect()->back();
               }
-
+              public function addToCart($id)
+              {
+          
+                  $product=Product::find($id);
+                  if($product)
+                  {
+                      $cart=session()->get('cart');
+          //            dd($cart);
+                      if(!$cart)
+                      {
+                          //step 1: cart empty
+                          //add to cart- first product
+                              $myCart[$id]=[
+                                'name'=>$product->name,
+                                'price'=>$product->price,
+                                'quantity'=>1,
+                                'subtotal'=>$product->price,//price x quantity
+                              ];
+          
+                            session()->put('cart',$myCart);
+                            Alert::toast()->success('Product added to the cart');
+          
+                            return redirect()->back();
+                      }
+          
+          
+                      //step 2:Cart not empty but product not exist
+                      //add to cart
+          
+                      if(!array_key_exists($id,$cart)){
+                          $cart[$id]=[
+                              'name'=>$product->name,
+                              'price'=>$product->price,
+                              'quantity'=>1,
+                              'subtotal'=>$product->price,//price x quantity
+                          ];
+          
+                          session()->put('cart',$cart);
+                          Alert::toast()->success('New product added to the cart');
+                          return redirect()->back();
+          
+                      }
+          
+                      //step 3 : cart not empty but product exist
+                      // quantity , subtotal update
+                      $cart[$id]['quantity']=$cart[$id]['quantity']+1;
+                      $cart[$id]['subtotal']=$cart[$id]['quantity'] * $cart[$id]['price'];
+                      session()->put('cart',$cart);
+          
+                      Alert::toast()->success('Cart successfully updated.');
+                      return redirect()->back();
+                    }
+          
+                    Alert::toast()->success('No Product Found.');
+                    return redirect()->back();
+          
+          
+          
+                      }
         }
